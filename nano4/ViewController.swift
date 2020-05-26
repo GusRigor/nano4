14dc9d83a4f5e34f02ab292d436009209eb99313
gustavo rigor <gusrigor@icloud.com>
2020-05-26 14:11:03 -0300

    update
    utilizar uma imagem a partir do url da mesma

--- nano4/ViewController.swift
+++ nano4/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
+let gatoDeChapeu = "https://i0.wp.com/www.hillsides.com.br/wp-content/uploads/2020/04/Gomaomi-animal-de-estimao-cachorro-gato-traje-cowboy-jeans-camisas-com-capuz-halloween-traje-macaco-32698217019-6334-600x600-1.jpg"
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageURLtest: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateImage()
+    }
+    
+    func updateImage(){
+        let urlImage = URL(string: gatoDeChapeu)!
+        imageURLtest.load(url: urlImage)
     }
 
+}
 
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
 }
 

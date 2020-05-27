1a4a77635666b2e6626a6fe3c5e45bad98720c09
gustavo rigor <gusrigor@icloud.com>
2020-05-27 16:48:24 -0300

    adicionando comentários

--- nano4/nano4/ViewController.swift
+++ nano4/nano4/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+//Adicionada a classe as extensões para o controle da tableview
+
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
 
@@ -21,18 +23,21 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    //Utilizado para carregar os dados da tableview assim que entramos na tela
     override func viewDidAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         fetchAndUpdateTable()
     }
     
+    //Essa função é usada para o quando a célula da tableView é selecionada, para o caso atual, estamos usando para editar as informações
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let filmSerie = dataSourceArray[indexPath.row]
         var nomeTextField: UITextField?
         var duracaoTextField: UITextField?
         var imgURLTextField: UITextField?
         
-        let mensagemDialogo = UIAlertController(title: "Adicionando Filme", message: "Atualizando o filme vc assistiu?", preferredStyle: .alert)
+        let mensagemDialogo = UIAlertController(title: "Atualizando o Filme", message: "Atualizando o filme vc assistiu?", preferredStyle: .alert)
         
         let ok = UIAlertAction(title: "Atualizar", style: .default, handler: {(action) -> Void in
             let nome = nomeTextField?.text
@@ -71,6 +76,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         self.present(mensagemDialogo, animated: true, completion: nil)
     }
     
+    
+    //Está função faz a remoção do item da lista da tableview e do banco de dados. Para o caso é fazer aquela arrastadinha para o lado
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete{
             let filmSerie = dataSourceArray[indexPath.row]
@@ -79,10 +86,12 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
     }
     
+    //Está funcão está olhando no array de dados quantas celulas precisa instanciar
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return dataSourceArray.count
     }
-       
+    
+    //Está função realiza o prenchimento da células com as informcações do banco de dados
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
         let filmSerie = dataSourceArray[indexPath.row]
@@ -96,11 +105,14 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
     }
     
+    //Está função atualiza a lista e realiza um pesquisa no banco de dados
     func fetchAndUpdateTable(){
         dataSourceArray = appDelegate.fetchRecords()
         tableViewGeneric.reloadData()
     }
     
+    
+    //Está função é referente ao botão do lado superior direito que realiza a adição de uma nova linha no banco de dados com as informações realizadas
     @IBAction func AddFilmSerie(_ sender: UIButton) {
         var nomeTextField: UITextField?
         var duracaoTextField: UITextField?

4c7bbcb6cf793d76aa37b4fa2ceb04ef83f6fc06
gustavo rigor <gusrigor@icloud.com>
2020-06-02 17:23:11 -0300

    Adicionada tela de usuário

---
+++ nano4/nano4/ViewControllers/ConfigUsuario.swift
@@ -0,0 +1,24 @@
+//
+//  ConfigUsuario.swift
+//  nano4
+//
+//  Created by Gustavo Rigor on 02/06/20.
+//  Copyright © 2020 Gustavo Rigor. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ConfigUsuario : UIViewController{
+    
+    @IBOutlet weak var imgUsuario: UIImageView!
+    
+    @IBOutlet weak var nomeUsuario: UILabel!
+    
+    @IBOutlet weak var darkmode: UISwitch!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

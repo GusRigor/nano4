b00503f4901ff9d1f71b05f3f402ff0909d6e830
gustavo rigor <gusrigor@icloud.com>
2020-06-03 23:06:09 -0300

    Adicionada a tela inicial

---
+++ nano4/nano4/ViewControllers/TelaPrincipalViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TelaPrincipalViewController.swift
+//  nano4
+//
+//  Created by Gustavo Rigor on 03/06/20.
+//  Copyright © 2020 Gustavo Rigor. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TelaPrincipal : UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}

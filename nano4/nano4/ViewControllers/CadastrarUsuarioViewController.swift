253688a7b5e088fc4d570cc0d310c4fd7a9a56b4
gustavo rigor <gusrigor@icloud.com>
2020-06-08 16:01:15 -0300

    Complementando o código.

--- nano4/nano4/ViewControllers/CadastrarUsuarioViewController.swift
+++ nano4/nano4/ViewControllers/CadastrarUsuarioViewController.swift
@@ -13,8 +13,45 @@ class CadastrarUsuarioViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        observer()
     
     }
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    func observer(){
+        NotificationCenter.default.addObserver(self, selector: #selector(mostraTeclado), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(escondeTeclado), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func mostraTeclado(notificacao: NSNotification){
+               if let keyboardSize = (notificacao.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue{
+                   if let duracao = notificacao.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? Double {
+
+                       UIView.animate(withDuration: duracao){
+                           let limites = UIScreen.main.bounds
+                           self.view.frame = CGRect(x: limites.origin.x, y: limites.origin.y, width: limites.width, height: limites.height - keyboardSize.height)
+                           self.view.layoutIfNeeded()
+                       }
+                   }
+               }
+           }
+
+           @objc func escondeTeclado(notificacao: NSNotification){
+               if let duracao = notificacao.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? Double {
+                   UIView.animate(withDuration: duracao){
+                       self.view.frame = UIScreen.main.bounds
+                       self.view.layoutIfNeeded()
+                   }
+               }
+           }
+
+           override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+              nomeTextField?.resignFirstResponder()
+           }
+           func textFieldShouldClear(_ textField: UITextField) -> Bool {
+               return true
+           }
     
     override func viewDidAppear(_ animated: Bool) {
         // Busca no UserDefaults o que estiver salvo na forkey nomeUsuario e coloca na constante nome
